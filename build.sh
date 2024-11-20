2fb708025bfc5ed37215fdca01d447e2c13011c0
P. Harvey <62023010+P-Harvey@users.noreply.github.com>
2024-11-19 23:32:47 -0500

    Add files via upload

---
+++ build.sh
@@ -0,0 +1,2 @@
+docker build -t shLLeM .
+docker run -d -p 8000:8000 shLLeM
---
+++ setup.sh
@@ -0,0 +1,2 @@
+pip install fastapi uvicorn transformers pydantic python-multipart sentence-transformers
+pip install torch  # or tensorflow
